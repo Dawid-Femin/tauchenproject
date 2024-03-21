83baf29f1502e7a629a5ed431508f96b33deb424
Dawid Femin <dawid.femin@gmail.com>
2024-03-21 18:33:05 +0100

    F1-1 - created based app with storybook

---
+++ src/stories/Components/Header/Header.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Header as H } from './Header';
+
+const meta = {
+    title: 'Example/Header',
+    component: H,
+    parameters: {
+    },
+    tags: ['autodocs'],
+    argTypes: {
+    },
+    args: { onClick: fn() },
+} satisfies Meta<typeof H>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+    },
+};
