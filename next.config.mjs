121b886c7ebca7caedc36f65792f5ebf98cd499a
Dawid Femin <dawid.femin@gmail.com>
2024-03-26 00:37:32 +0100

    F1-1 - package.json file & package-lock.json & next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    publicRuntimeConfig: {
+        basePath: process.env.BASE_PATH || '',
+    },
+};
 
 export default nextConfig;
